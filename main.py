a7d3f3e44a1dace0634651dd096877dccf82ca78
Nick <nfsweerts@gmail.com>
2021-04-29 12:50:42 -0600

    Moved Readme

--- main.py
+++ main.py
@@ -3,11 +3,25 @@ import os
 
 pygame.font.init()
 
-FPS = 30
+FPS = 10
 WIDTH, HEIGHT = 900, 900
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("RockPaperScissors")
 
 def main():
-
+    clock = pygame.time.Clock()
+    run = True
+    front_page = True
+    wins = [0, 0]
+    Numofplayers = 1
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+        
+        clock.tick(FPS)
+        
     
 
 if __name__ == '__main__':
